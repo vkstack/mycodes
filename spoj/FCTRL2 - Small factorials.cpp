f85831cd685c35449aa0585a484db384013cb8c3
vkstack <kvaj007@gmail.com>
2016-02-11 04:09:24 +0530

    .

--- spoj/FCTRL2 - Small factorials.cpp
+++ spoj/FCTRL2 - Small factorials.cpp
@@ -15,16 +15,17 @@ public:
     }
     bigInt operator*(const bigInt& b){
         bigInt a= *this;
-        int al=strlen(a.val),bl=strlen(b.val),i,j,k,x,carry=0,pos=0;
+        int al=strlen(a.val),bl=strlen(b.val),i,j,k,x,y,z,carry=0,pos=0;
         char result[10001];
         for(i=0;i<10001;i++)result[i]='0';
         for(i=al-1;i>=0;i--){
             x=a.val[i]-'0';
             k=pos++;
             for(j=bl-1;j>=0;j--){
-                x=result[k]-'0'+(x*(b.val[j]-'0'))+carry;
-                carry=x/10;
-                result[k++]='0'+x%10;
+                y=b.val[j]-'0';
+                z=result[k]-'0'+(x*y)+carry;
+                carry=z/10;
+                result[k++]='0'+z%10;
             }
             while(carry){
                 result[k++]+=carry%10;
